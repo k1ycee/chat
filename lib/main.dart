88f7cfa558bc83bce91f44c06fbc0477e98db341
Denzel <chiagozieani145@gmail.com>
2020-01-21 18:45:17 +0100

    Make yarns Na eh Paddy

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text("Chit Chat", style: TextStyle(color: Colors.green, fontSize: 20, fontWeight: FontWeight.w300),),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+                 "Please A chat app wants to be here Please",
+                 style: TextStyle(color: Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
